6a25ce92d616e2bd7c403cbc25c020a75f6385b8
Patryk Czemierowski <patryk.czemierowski@gmail.com>
2023-01-07 21:00:01 +0100

    articles stored in models and connected to templates

--- plasma/plasma_site/models.py
+++ plasma/plasma_site/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    text = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
--- plasma/plasma_site/views.py
+++ plasma/plasma_site/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Article
 
 def index(request):
-    return render(request, 'plasma_site/index.html', {} )
+    intro = Article.objects.get(title='intro')
+    party_des1 = Article.objects.get(title='party_des1')
+    party_des2 = Article.objects.get(title='party_des2')
+    context = {
+        'intro': intro,
+        'party_des1': party_des1,
+        'party_des2': party_des2
+    }
+
+    return render(request, 'plasma_site/index.html', context)
